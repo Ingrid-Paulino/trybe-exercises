fdce157449999491c5f09997654670ba9e25b9cb
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-07-19 18:47:47 -0300

    C1 - Adicionado atividades do dia 6.2, 6.3 e 6.4

--- exercise_6.1/exercise/script.js
+++ exercise_6.1/exercise/script.js
@@ -1,10 +1,379 @@
 //estados
-const estados = ['selecione','Acre','Alagoas','Amapá', 'Amazonas', 'Bahia', 'Ceará','Espírito Santo' , 'Goiás', 'Maranhão', 'Mato Grosso','Mato Grosso do Sul','Minas Gerais', 'Pará','Paraíba','Paraná','Pernambuco','Piauí', 'Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Distrito Federal']; 
+const cityTotal = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará','Paraíba','Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina','São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal']; 
 
-const estado = document.getElementById('estado');
-
-for(let index = 0; index < estados.length; index += 1) {
+const cityId = document.getElementById('estado');
+for(let index = 0; index < cityTotal.length; index += 1) {
   const createOption = document.createElement('option');
-  estado.appendChild(createOption).innerText = estados[index];
-  select.appendChild(createOptions).value = estados[index];
+  createOption.innerHTML = cityTotal[index];
+  cityId.appendChild(createOption);
 }
+
+
+
+
+// Estudar mais esse codigo
+//fonte: https://app.betrybe.com/course/fundamentals/html-forms/solutions
+// ---------------------------------------------------------------
+// Capiturar o evento de click do botao para cancelar o fluxo aotomatico
+// botão 
+
+//  objeto com tudo que sera verificado
+let inputs = {
+  name: {
+    maxLength: 40,
+    required: true,
+  },
+  email: {
+    maxLength: 50,
+    required: true
+  },
+  cpf: {
+    maxLength: 11,
+    required: true
+  },
+  address: {
+    maxLength: 200,
+    required: true
+  },
+  city: {
+    maxLength: 28,
+    required: true,
+  },
+  state: {
+    type: 'select',
+    required: true,
+  },
+  houseType: {
+    type: 'radio',
+    required: true,
+  },
+  resume: {
+    maxLength: 1000,
+    required: true,
+  },
+  role: {
+    maxLength: 40,
+    required: true,
+  },
+  roleDescription: {
+    maxLength: 500,
+    required: true,
+  },
+  startDate: {
+    type: 'date',
+    required: true,
+  }
+}
+
+function defaultValidation(input, name){
+  let trimmed = input.value.trim();
+  let validation = inputs[name];
+
+  if(validation.required && trimmed.length === 0){
+    return false;
+  }
+
+  if(validation.maxLength && trimmed.length > validation.maxLength){
+    return false;
+  }
+  
+  return true;
+}
+
+// data
+
+function dateValidation(input, name){
+  if(input.value.length === 0){
+    return {
+      message: 'A data não foi preenchida!'
+    }
+  }
+
+
+  let regex = /^\d\d\/\d\d\/\d\d\d\d$/;
+  
+  if(!regex.test(input.value)){
+    return {
+      message: 'Data: Formato inválido'
+    };
+  }
+
+  let splitted = input.value.split('/');
+  let day = splitted[0];
+  let month = splitted[1];
+  let year = splitted[2];
+
+  if(day < 0 || day > 30){
+    return {
+      message: 'Dia inválido'
+    };
+  }
+
+  if(month < 0 || month > 12){
+    return {
+      message: 'Mês inválido'
+    }
+  }
+
+  if(year < 0) {
+    return {
+      message: 'Ano inválido'
+    };
+  }
+
+  return true;
+}
+
+// lista de estados
+function getSelectedOption(select){
+  return select.options[select.selectedIndex];
+}
+
+function selectValidation(select, name){
+  let option = getSelectedOption(select);
+  let validation = inputs[name];
+
+  if(validation.required && (!option || option.disabled)){
+    return false;
+  }
+
+  return true;
+}
+
+// moradia 
+function radioValidation(radio, name){
+  let checked = document.querySelector(`[name=${name}]:checked`)
+  
+  if(checked === null){
+    return false;
+  } 
+
+  return true;
+}
+
+let validationStrategies = {
+  default: defaultValidation,
+  date: dateValidation,
+  select: selectValidation,
+  radio: radioValidation,
+}
+
+function validateInput(inputName){
+  let inputType = inputs[inputName].type;
+  let input = document.querySelector(`[name=${inputName}]`)
+
+  if(inputType){
+    let validationFunction = validationStrategies[inputType];
+    return validationFunction(input, inputName);
+  }
+
+  return validationStrategies.default(input, inputName);
+}
+
+function renderErrorMessages(messages){
+  let form = document.querySelector('#cv-form');
+  let messageDiv = document.createElement('div');
+  messageDiv.className = 'error';
+  form.prepend(messageDiv);
+
+  for(let message of messages){
+    let p = document.createElement('p');
+    p.innerText = message;
+
+    messageDiv.appendChild(p);
+  }
+}
+
+function defaultRendering(input){
+  let p = document.createElement('p');
+  p.innerText = input.value;
+
+  return p;
+}
+
+function radioRendering(input){
+  let p = document.createElement('p');
+  let name = input.getAttribute('name');
+  let checked = document.querySelector(`[name=${name}]:checked`);
+
+  if(checked){
+    p.innerText = checked.value;
+  }
+
+  return p;
+}
+
+function defaultRendering(input){
+  let p = document.createElement('p');
+  p.innerText = input.value;
+
+  return p;
+}
+
+function radioRendering(input){
+  let p = document.createElement('p');
+  let name = input.getAttribute('name');
+  let checked = document.querySelector(`[name=${name}]:checked`);
+
+  if(checked){
+    p.innerText = checked.value;
+  }
+
+  return p;
+}
+
+function selectRendering(input){
+  let p = document.createElement('p');
+  let option = getSelectedOption(input)
+  p.innerText = option.value;
+  
+  return p;
+}
+
+let renderStrategies = {
+  default: defaultRendering,
+  radio: radioRendering,
+  select: selectRendering,
+}
+
+function renderData(){
+  let dataDiv = document.createElement('div');
+  dataDiv.className = 'data';
+
+  let form = document.querySelector('#cv-form');
+  form.prepend(dataDiv);
+
+  for(let name in inputs){
+    let inputType = inputs[name].type;
+    let input = document.querySelector(`[name=${name}]`);
+
+    let element;
+
+    if(renderStrategies[inputType]){
+      element = renderStrategies[inputType](input, dataDiv)
+    } else {
+      element = renderStrategies.default(input, dataDiv)
+    }
+
+    dataDiv.appendChild(element);
+  }
+}
+
+function validateData(){
+  let validationsList = {};
+
+  for(let inputName in inputs){
+    let isValid = validateInput(inputName);
+    validationsList[inputName] = isValid;
+  }
+  
+  let counter = 0;
+  let messages = [];
+
+  for(let index in validationsList){
+    if(validationsList[index] === false){
+      counter += 1;
+    }
+
+    if(validationsList[index].message){
+      counter += 1;
+      messages.push(validationsList[index].message);
+    }
+  }
+
+  return {
+    errorQtd: counter,
+    messages,
+  }
+}
+
+function clearDivs(){
+  let errorDivs = document.querySelectorAll('.error');
+
+  for(div of errorDivs){
+    div.remove();
+  }
+
+  let dataDiv = document.querySelector('.data');
+
+  if(dataDiv){
+    dataDiv.remove();
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let validation = validateData();
+
+  clearDivs();
+  
+  if(validation.errorQtd === 0){
+    renderData();
+  } else {
+    validation.messages.unshift('Dados Inválidos')
+
+    renderErrorMessages(validation.messages)
+  }
+}
+
+function clearFields() {
+  let formElements = document.querySelectorAll('input');
+  let textArea = document.querySelector('textarea')
+  let div = document.querySelectorAll('.div-curriculum');
+  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+    let userInput = formElements[index];
+    userInput.value = '';
+    textArea.value = '';
+    div[index].innerText = '';
+  }
+}
+
+window.onload = function () {
+  createStateOptions();
+  let submitButton = document.querySelector('.submit-button');
+  submitButton.addEventListener('click', handleSubmit);
+
+  let clearButton = document.querySelector('.clear-button');
+  clearButton.addEventListener('click', clearFields)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fazer a validação desse jeito - mas entenda a de cima
+
+
+
+// function lidaComSubmit(event) {
+//   event.preventDefault(); 
+
+//   // validar os dados
+//   let name = document.querySelector('[name=name')
+
+//   // verificar maxLength e Require
+//   // console.log(name.value) //consigo recuperar o valor
+  // if (name.value.length > 40 || name.value.length === 0) {
+  //   alert('Nome inválido')
+  // } else {
+  //   alert('Nome válido')
+  // }
+// }
+// // event guarda as informaçoes do evento (o click)
+// // preventDefoult previne o comportamento padrão do botão , nesse caso não valida e envia. So valida.
+
+// window.onload = function () {
+
+//   let button = document.querySelector('.submit-button');
+//   button.addEventListener('click', lidaComSubmit)
+// }
+

27f51de903fc7692ad7ce3820026d0a251571408
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-08-31 15:38:27 -0300

    C1 - Adicionado dia 11.1

---
+++ exercises_11.1/aoVivoClass/src/HelloWorld.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+// primeira letra tem que ser maiuscula
+class HelloWorld extends React.Component {
+  render() {
+
+    const element = (
+      <h3>Usar React vai ser Maravilhoso, AMEM</h3>
+    );
+
+    //a className abaixo nn funcionou - nao sei porque
+    const element2 = React.createElement(
+      'h1',
+      {className: 'oi'},
+      'Vai com tudo ingridzinha'
+    );
+
+    const currentLearning = (subject) => {
+      return (<h2> Eu estou aprendendo {subject} </h2>)
+    };
+  
+  return (
+  // Se ouver mais de um conteudo, tem que ter um elemento pai
+  // Podemos usar tags semanticas, Fragment 0u <></>
+  <> 
+    {/* React não usa class e sim className, pois class é uma palavra reservada para o react*/}
+    <h1 className='hello-world'>Hello, World!</h1>
+    <p>Esse é o meu primeiro App React</p>
+    {element}
+    {element2}
+    <section>
+      <p>Esse é o meu primeiro App React</p>
+      {currentLearning('React')}
+    </section>
+  </>  
+  );
+}
+};
+
+export default HelloWorld;

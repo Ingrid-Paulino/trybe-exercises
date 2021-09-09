56179db32970e59d3fea9777cf69dcf9386df06f
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-09 15:11:23 -0300

    Adicionado identaçao

--- exercises_11.1/aoVivoClass/src/HelloWorld.js
+++ exercises_11.1/aoVivoClass/src/HelloWorld.js
@@ -38,3 +38,16 @@ class HelloWorld extends React.Component {
 };
 
 export default HelloWorld;
+
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);

b0cc0495e90267696a6d1e1d874ee4c4715267a9
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-24 11:54:21 -0300

    :)

--- exercise.12.1/aulaAoVivo/src/App.js
+++ exercise.12.1/aulaAoVivo/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import colors from './colorsData'
-import './App.css';
+import React from "react";
+import colors from "./colorsData";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
@@ -8,16 +8,16 @@ class App extends React.Component {
     //guarda as informaçoes do filter
     this.state = {
       //vamos guardar aqui o valor do input
-      filterText: '',
+      filterText: "",
     };
     //estou possibilitando do hand.. acessar o this
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    // this.state.filterText = event.target.value; //eRRRRRRAAAAADOOO, PERIGO 
+    // this.state.filterText = event.target.value; //eRRRRRRAAAAADOOO, PERIGO
     // troca o valor do estado
-    this.setState({ filterText: event.target.value })
+    this.setState({ filterText: event.target.value });
     console.log(event.target.value);
   }
 
@@ -27,18 +27,16 @@ class App extends React.Component {
     return (
       <main>
         <h1>Filtre as cores:</h1>
-        <input type="text" 
-        value={ filterText } 
-        onChange={ this.handleChange } />
+        <input type="text" value={filterText} onChange={this.handleChange} />
         <ul>
           {colors
-          .filter((element) => element.name.includes(filterText))
-          .map((color) => (
-            <li key={ color.hex }> 
-              <div style={ { background: color.hex } } className="color-display" />
-              {color.name }
-            </li>
-        ))}
+            .filter((element) => element.name.includes(filterText))
+            .map((color) => (
+              <li key={color.hex}>
+                <div style={{ background: color.hex }} className="color-display" />
+                {color.name}
+              </li>
+            ))}
         </ul>
       </main>
     );

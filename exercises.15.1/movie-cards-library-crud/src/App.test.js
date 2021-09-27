f5c0792842f74914ce01981b41ba798b5128cb56
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-09-27 18:38:28 -0300

    Adicionado conteudo do bloco 15.1 e 15.2

---
+++ exercises.15.1/movie-cards-library-crud/src/App.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, screen, fireEvent, waitForElementToBeRemoved } from '@testing-library/react';
+import App from './App';
+
+describe('MovieList', ()=>{
+  test('Renderiza "Carregando..." quando a aplicação é iniciada', () => {
+    const { getByText } = render(<App/>);
+
+    expect(getByText(/carregando/i)).toBeInTheDocument();
+    // expect(getByText('maitte')).toBeInTheDocument(); testando o teste para ver se ele esta funcionando, aqui ele da falso, e o teste esta correto. nao esta dando falso positivo
+    // expect(getByText(/carReganDo/i')).toBeInTheDocument();  é sencitive, não importa se esta em maiuscula ou minuscula, e nao é necessario colocar a frase exatamente como esta
+  });
+
+  it('Exibe o primeiro filme', async () => {
+    render(<App/>);
+
+    // Função retirada da documentação do RTL, estou aprendendo a usar
+    // https://testing-library.com/docs/react-testing-library/cheatsheet
+    await waitForElementToBeRemoved(() =>
+      screen.getByText(/carregando/i),
+      { timeout: 3000 },
+    );
+
+    expect(screen.getByText('Kingsglaive')).toBeInTheDocument();
+    expect(screen.getByText('Final Fantasy')).toBeInTheDocument();
+    expect(screen.getByText('Ghost In The Shell')).toBeInTheDocument();
+  });
+
+  test('Renderiza a página de detalhes ao clicar em "Ver detalhes"', async () => {
+    render(<App/>);
+
+    await waitForElementToBeRemoved(() =>
+      screen.getByText(/carregando/i),
+      { timeout: 3000 },
+    );
+
+    const detailsButton = screen.getByTestId(/kingsglaiveDetails/i);
+    fireEvent.click(detailsButton);
+
+    await waitForElementToBeRemoved(() =>
+      screen.getByText(/carregando/i),
+      { timeout: 3000 },
+    );
+
+    expect(screen.getByText(/genre/i)).not.toBeNull();
+  });
+});
